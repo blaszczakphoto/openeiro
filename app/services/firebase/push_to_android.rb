423cd1655702dd7480bc379e1ab4bc2ce5629c15
Mariusz Błaszczak <mariusz.blaszczak@akra.net>
2018-08-23 11:32:16 +0200

    Use registration_ids from db

--- app/services/firebase/push_to_android.rb
+++ app/services/firebase/push_to_android.rb
@@ -2,7 +2,7 @@ module Firebase
   class PushToAndroid
     SERVER_KEY = ENV.fetch('FIREBASE_APP_SERVER_KEY')
     def call
-      registration_ids = ['d3YQvfRi5tk:APA91bF0eGGlFJzbZrdH3P8CDKlMTOYRfhDjGHCAQsXApskYkCXar6VLwAw8zHF2jknB4GQvRcedDOmKJ84h-hy4LIFp4rFcUnGmKruecP2xxsIqCNzCBbWpiflJkmEBQUbkzhCvYpXy']
+      registration_ids = User.all.map(&:registration_id)
       options = { data: { state: Doors.last_event.attributes }, collapse_key: 'updated_score' }
       fcm = FCM.new(SERVER_KEY)
       fcm.send(registration_ids, options)
